91861a18699fe94ca432464780f0228567feef59
higashi <k-higashi@nihongas.co.jp>
2024-01-05 08:21:33 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: '#ebf8ff',
+          500: '#4299e1',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
